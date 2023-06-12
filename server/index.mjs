b7d5dfb74abdb4adb3a846a67449f661999fd6ec
James McGahn <jmcgahn@gmail.com>
2023-06-12 16:33:16 -0400

    add protect mw, apollo context

--- server/index.mjs
+++ server/index.mjs
@@ -10,6 +10,7 @@ import typeDefs from './graphql/typedefs.mjs';
 import resolvers from './graphql/resolvers.mjs';
 import 'dotenv/config';
 import connectDB from './config/db.mjs';
+import protect from './middleware/protect.mjs';
 
 const PORT = process.env.PORT || 5000;
 
@@ -23,12 +24,17 @@ connectDB();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: {},
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 await server.start();
 
-app.use(cors(), bodyParser.json(), expressMiddleware(server));
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req, res }) => ({ user: await protect(req) }),
+  }),
+);
 
 // trunk-ignore(eslint/no-promise-executor-return)
 await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve)).then(
---
+++ server/middleware/protect.mjs
@@ -0,0 +1,23 @@
+import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.mjs';
+
+const protect = async (req, res) => {
+  const { authorization } = req.headers;
+  let token;
+
+  if (authorization && authorization.startsWith('Bearer')) {
+    token = authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return false;
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  const user = await User.findById(decoded.id);
+
+  return user;
+};
+
+export default protect;
