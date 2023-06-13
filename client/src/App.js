473f5557c1c9e0092aa558ac8b27cac2a4a896ba
James McGahn <jmcgahn@gmail.com>
2023-06-12 22:29:49 -0400

    add apollo config and bootstrap css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,48 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  return <>hello world</>;
+  const cache = new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          User: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+          Tickets: {
+            merge(existing, incoming) {
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache,
+  });
+
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+
+              <Route path="*" element={<>notfound</>} />
+            </Routes>
+          </div>
+        </Router>
+      </ApolloProvider>
+    </>
+  );
 }
 
 export default App;
